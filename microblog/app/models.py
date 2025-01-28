556e16aad1cf8db6295a8dd67dd5c8e0331138e8
pav9551 <pav9551@yandex.ru>
2025-01-28 17:28:14 +0300

    added CHAPTER6 PROFILE PAGE AND AVATARS

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
+from datetime import  timedelta
 
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,19 +12,22 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.Text, nullable=False)  # Используем Text вместо String
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=lambda: datetime.utcnow() + timedelta(hours=3))
     def __repr__(self):
         return '<User {}>'.format(self.username)
     def set_password(self, password):
 	    self.password_hash = generate_password_hash(password)
-
-
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, index=True, default=lambda: datetime.utcnow() + timedelta(hours=3))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     def __repr__(self):
         return '<Post {}>'.format(self.body)
